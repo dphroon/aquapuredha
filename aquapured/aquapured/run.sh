8ef73ff8fdc8442945919dc0e52e99cad1a2a2a8
dphroon <77174861+dphroon@users.noreply.github.com>
2021-07-22 23:03:10 +1000

    Update run.sh

--- aquapured/aquapured/run.sh
+++ aquapured/aquapured/run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 python ./aquapured/json2cfg.py /data/options.json ./aquapured/release/aquapured.conf
-./aquapured/release/aquapured -d -c ./aquapured/release/aquapured.conf
+cat ./aquapured/release/aquapured.conf
+cd aquapured
+./release/aquapured -d -c ./release/aquapured.conf
 echo "Hi... you weren't meant to get here"
